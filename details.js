51ab1fa7ffbb5456e763551e8ae61ad3ccb642eb
KingGamer-YT <ridwanadenubi885@gmail.com>
2024-09-18 08:37:54 +0100

    changes

--- details.js
+++ details.js
@@ -1,15 +1,11 @@
-// Retrieve the stored JSON data from local storage
-jsonData = localStorage['jsonData'];
 
-// Commented out console logs for debugging the JSON data
+jsonData = localStorage['jsonData'];
 // console.log("json");
 // console.log(JSON.parse(jsonData));
 
-// Parse the JSON data into a JavaScript object
 var countriesData = JSON.parse(jsonData);
 
-// Loop through the array of countries
-for (var i = 0; i < countriesData.length; i++) {
-    // Log the name of each country to the console
+
+for(var i=0;i<countriesData.length;i++){
     console.log(countriesData[i].name);
 }
